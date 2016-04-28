c4360f7630fdf0154464652a98c38b46645bbe77
OJ LaBoeuf <orangepeelbeef@gmail.com>
2016-04-28 02:42:39 -0700

    added redis hotqueue output for scraped data
    will have another script that runs with the webapp to pull items off the redis queue and stuff them into mysql.
    doing this with redis so that we can easily scale if necessary, and to prevent mysql from blocking the scraper from moving forward.
    
    This also allows us to potentially feed data in from other sources not just BDD.

--- bdd_raper_scraper.py
+++ bdd_raper_scraper.py
@@ -1,38 +1,70 @@
 #!/usr/bin/python2
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+import json
 import time
 import pprint
+from hotqueue import HotQueue
 
 
 #global variables
-debug = True
+
 next_enabled = True
-
-
 data_queue = []
 
-#config settings for each category for url , table, type mapping
-cfg = {
-    'PrimaryWeapon': {'url': 'weapon', 'table': 'Weapon', 'type': 'primary_weapon'},
-    'SecondaryWeapon': {'url': 'subweapon', 'table': 'Weapon', 'type': 'secondary_weapon'},
-    'Fish': {'url': 'items/fish', 'table': 'MainItem', 'type': 'seafood'}
+#app configuration
+appcfg = {
+    'debug': True,
+    'redis': True,
+    'redis_host': 'localhost',
+    'redis_port': 6379,
+    'redis_db': 0,
+    'queue_name': 'bdd'
 }
 
 
+#config settings for each category for url , table, type mapping
+catmap = {
+    'PrimaryWeapon': {'url': 'weapon', 'table': 'Weapon', 'type': 'primary_weapon'},
+    'SecondaryWeapon': {'url': 'subweapon', 'table': 'Weapon', 'type': 'secondary_weapon'},
+    'Fish': {'url': 'items/fish', 'table': 'MainItem', 'type': 'seafood'},
+    'ProdRecipes': {'url': 'designs', 'table': 'Recipes', 'type': 'production'},
+    'CookingRecipes': {'url': 'recipes/culinary', 'table': 'Recipes', 'type': 'cooking'}
+}
+
+#setup ublock origin adblocker cuz BDDatabase is horribly full of ads/adware
 chrome_options = Options()
 chrome_options.add_extension('ublock-origin_1_6_8.crx')
 
 #language of site
 lang = 'us'
 
-
+#set up the data dumper style output for debuggins
 pp = pprint.PrettyPrinter(indent=4)
 
-#debug print function
+#debug print function so final version is more quiet :)
 def dp(message):
-    if debug:
+    global appcfg
+    if appcfg['debug']:
         print message
+
+
+def parse_icon_mats(mats):
+    mat_data = []
+    # this needs optimization, it takes quite a while to get all the data out of the mats
+    for mat in mats:
+        # dp(mat.get_attribute('innerHTML'))
+        mat_img = mat.find_element_by_tag_name('img').get_attribute('src')
+        try:
+            mat_qty = mat.find_element_by_css_selector('div.quantity_small.nowrap').text
+        except NoSuchElementException:
+            mat_qty = "1"
+        mat_url = mat.find_element_by_tag_name('a').get_attribute('href')
+        mat_data.append({'img': mat_img, 'qty': mat_qty, 'url': mat_url})
+    return mat_data
+
+
 
 #click on the Next button if it isn't disabled and kick off another parsing.
 def next_page(config):
@@ -53,12 +85,28 @@ def next_page(config):
         return
 
 
+def output_category(cat,data):
+    global redis_enabled, appcfg
+
+    pp.pprint(data)
+    print "{cat} Entries: {size}".format(cat=cat,size=len(data))
+    if appcfg['redis']:
+        queue = HotQueue(
+            appcfg['queue_name'], host=appcfg['redis_host'], port=appcfg['redis_port'], db=appcfg['redis_db']
+        )
+        for item in data:
+            queue.put(item)
+
+
+
 #parse out the data we need
 def parse_category(config,rows):
     global data_queue
     table = config['table']
+    #WeaponTable parsing
     if table == 'Weapon':
         for row in rows:
+            #cols  id, image, title, level, ap, dp
             cols = row.find_elements_by_tag_name('td')
             url = cols[2].find_element_by_tag_name('a')
             img = cols[1].find_element_by_tag_name('img')
@@ -67,14 +115,30 @@ def parse_category(config,rows):
                     'name': url.text
                     }
             data_queue.append(data)
-    #MainItem table has no AP/DP so it needs its own parsing
+    #MainItemTable parsing
     elif table == 'MainItem':
         for row in rows:
+            #cols  id, image, title, level,
             cols = row.find_elements_by_tag_name('td')
             url = cols[2].find_element_by_tag_name('a')
             img = cols[1].find_element_by_tag_name('img')
             data = {'type': config['type'], 'bdd_id': cols[0].text, 'img': img.get_attribute('src'),
                     'url': url.get_attribute('href'), 'level': cols[3].text, 'name': url.text
+                    }
+            data_queue.append(data)
+    #RecipeTable parsing
+    elif table == 'Recipes':
+        for row in rows:
+            #cols   id, image, title, skill level, materials, products
+            cols = row.find_elements_by_tag_name('td')
+            url = cols[2].find_element_by_tag_name('a')
+            img = cols[1].find_element_by_tag_name('img')
+            #mats are displayed as icons with quantity in a separate div
+            mats = cols[4].find_elements_by_css_selector('div.iconset_wrapper_medium')
+            mat_data = parse_icon_mats(mats)
+            data = {'type': config['type'], 'bdd_id': cols[0].text, 'img': img.get_attribute('src'),
+                    'url': url.get_attribute('href'), 'skill_level': cols[3].text, 'name': url.text,
+                    'mats': mat_data
                     }
             data_queue.append(data)
     else:
@@ -91,7 +155,7 @@ def snarf_category(config):
     br.get("http://www.bddatabase.net/{lang}/{url}".format(lang=lang,url=url))
     table = config['table']
     br.find_element_by_xpath("//select[@name='{table}Table_length']/option[text()='100']".format(table=table)).click()
-    time.sleep(3)
+    time.sleep(5)
     rows = br.find_elements_by_xpath("//table[@id='{table}Table']/tbody/tr".format(table=table))
 
     parse_category(config,rows)
@@ -100,13 +164,14 @@ def snarf_category(config):
 
 
 
-br = webdriver.Chrome(chrome_options=chrome_options)
-#categories = [ 'Fish', 'Weapon', 'SubWeapon' ]
-categories = ['Fish', 'PrimaryWeapon', 'SecondaryWeapon']
-for cat in categories:
-    snarf_category(cfg[cat])
 
-pp.pprint(data_queue)
-print "Entries: {size}".format(size=len(data_queue))
+br = webdriver.Chrome(chrome_options=chrome_options)
+
+#categories = ['Fish', 'PrimaryWeapon', 'SecondaryWeapon']
+categories = ['ProdRecipes','CookingRecipes']
+for cat in categories:
+    snarf_category(catmap[cat])
+    output_category(cat, data_queue)
+    data_queue=[]
 
 br.quit()
